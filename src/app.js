cb0bb4dc676c786f6a90cf5c9200c5b9f9498965
sunnysakib <sakibur.rahaman033@gmail.com>
2023-06-25 21:52:26 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const planetsRouter = require('./routes/planets/planets.router');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+const launchesRouter = require('./routes/launches/launches.routers');
+
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(planetsRouter);
+app.use(launchesRouter);
+
+// app.get('*/',(req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+// })
+
+module.exports = app
---
+++ src/routes/launches/launches.controller.js
@@ -0,0 +1,6 @@
+const getAllLaunches = require ('../../models/launches.model');
+function httpGetAllLaunches(req, res) {
+    res.status(200).json(getAllLaunches());
+}
+
+module.exports = httpGetAllLaunches;
