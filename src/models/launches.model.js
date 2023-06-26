0a93a68b899b46686d7be7710bc85c7b0c48e151
sunnysakib <sakibur.rahaman033@gmail.com>
2023-06-26 12:22:56 +0600

    create post method for new launch

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -1,19 +1,35 @@
 const launches = new Map();
+
+let latestFlightNumber = 100;
+
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
-    customer: ['ZTM', 'nasa'],
-    upcoming: true,
-    success: true,
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "Kepler-442 b",
+  customer: ["ZTM", "nasa"],
+  upcoming: true,
+  success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
-function getAllLaunches(){
-     return Array.from(launches.values())
+function getAllLaunches() {
+  return Array.from(launches.values());
 }
 
-module.exports = getAllLaunches;
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customer: ['ZTM', 'NASA'],
+      upcoming: true,
+      success: true,
+    })
+  );
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -1,6 +1,16 @@
-const getAllLaunches = require ('../../models/launches.model');
+const {getAllLaunches,addNewLaunch} = require ('../../models/launches.model');
+
+// GET
 function httpGetAllLaunches(req, res) {
-    res.status(200).json(getAllLaunches());
+    return res.status(200).json(getAllLaunches());
 }
 
-module.exports = httpGetAllLaunches;
+// POST
+function httpAddNewLaunch (req, res) {
+    const launch = req.body;
+    launch.launchDate = new Date(launch.launchDate)
+    addNewLaunch(launch);
+    return res.status(201).json(launch)
+}
+
+module.exports = {httpGetAllLaunches, httpAddNewLaunch};
