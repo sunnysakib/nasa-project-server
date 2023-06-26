52dcae449a53535e24d7c3661da8c33d0ad4ca5b
sunnysakib <sakibur.rahaman033@gmail.com>
2023-06-26 13:37:32 +0600

    launch post request validation

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -8,7 +8,19 @@ function httpGetAllLaunches(req, res) {
 // POST
 function httpAddNewLaunch (req, res) {
     const launch = req.body;
+
+    if(!launch.mission || !launch.rocket || !launch.target 
+    || !launch.launchDate){
+        return res.status(400).json({
+            error: 'Missing required launch data',
+        })
+    }
     launch.launchDate = new Date(launch.launchDate)
+    if(launch.launchDate.toString() === "Invalid Date"){
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        })
+    }
     addNewLaunch(launch);
     return res.status(201).json(launch)
 }
