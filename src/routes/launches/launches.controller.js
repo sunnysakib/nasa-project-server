44f19c5ccfdda4a842264dfab8d27f148ff3000a
sunnysakib <sakibur.rahaman033@gmail.com>
2023-07-19 23:42:15 +0600

    paginating spaceX launches API

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -1,8 +1,12 @@
 const {getAllLaunches, existsLaunchWithId, abortLaunchById, scheduleNewLaunch} = require ('../../models/launches.model');
+const getPagination = require('../../utils/query');
+
 
 // GET
 async function httpGetAllLaunches(req, res) {
-    return res.status(200).json(await getAllLaunches());
+    const { skip, limit } = getPagination(req.query);
+    const launches = await getAllLaunches(skip, limit);
+    return res.status(200).json(launches);
 }
 
 // POST
