e51ebc6972cd759a2a1fb2c99201af22abf57402
sunnysakib <sakibur.rahaman033@gmail.com>
2023-10-15 22:38:05 +0600

    a

--- src/app.js
+++ src/app.js
@@ -9,7 +9,11 @@ app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use('/v1', api)
+app.use('/', api)
+
+api.get("/", (req, res) => {
+    res.send("Hello!!");
+  });
 
 // app.get('*/',(req, res) => {
 //     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
